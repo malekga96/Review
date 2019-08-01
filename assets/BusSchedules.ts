ff2feda23cc1ed15add4baf34ba30a9630851997
malekga96 <52546645+malekga96@users.noreply.github.com>
2019-08-01 10:18:50 -0400

    Add files via upload

---
+++ assets/BusSchedules.ts
@@ -0,0 +1,7 @@
+import {BusSchedule} from '../app/BusSchedule';
+
+export const BUSSCHEDULES: BusSchedule [] =
+[
+    {route:'route A', timeLeave: '12:00pm', timeArrive:'02:00pm'},
+    {route:'route B', timeLeave: '01:30pm', timeArrive:'02:30pm'},
+];
